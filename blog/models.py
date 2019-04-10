9b4f39c79901de0602bf55f84325cddd0f857c30
Yousif Al-Bayaty <y_al-bayaty@fanshaweonline.ca>
2019-04-10 17:01:43 -0400

    -Added super user -Created Admin page -Added the models to the Admin page

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,15 @@ class Author(models.Model):
     name = models.CharField(max_length=200)
     bio = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=2000)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     created_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
